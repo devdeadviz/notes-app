a81c93d102909ab63f2388289727ee829cbe823a
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-02 01:47:35 +0530

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Mockman from "mockman-js";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Navbar } from "./components";
 import { LandingPage } from "./pages";
 
 const App = () => {
+  const location = useLocation();
   return (
     <>
+      {location.pathname === "/" ? "" : <Navbar />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/mockman" element={<Mockman />} />
