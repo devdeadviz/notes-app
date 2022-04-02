095058d34e5424a46d348d66d9763481d6441b5e
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-03 03:13:22 +0530

    feat: add search component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,6 +5,17 @@ const HomePage = () => {
   return (
     <section className="homepage-wrapper flex">
       <Sidebar />
+      <section className="homepage-main-section flex flexCol flexAlignItemsCenter pt-2">
+        <div className="homepage-search-input-wrapper flex flexAlignItemsCenter">
+          <i class="fa-solid fa-magnifying-glass"></i>
+          <input
+            className="homepage-search-input"
+            type="text"
+            placeholder="Search"
+          />
+          <i class="fa-solid fa-bars"></i>
+        </div>
+      </section>
     </section>
   );
 };
