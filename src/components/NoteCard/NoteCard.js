86a7ca7e88f7b0e2e8c8709febad13449d9df218
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-04 02:52:13 +0530

    fix: update class to className

--- src/components/NoteCard/NoteCard.js
+++ src/components/NoteCard/NoteCard.js
@@ -2,22 +2,22 @@ import "./NoteCard.css";
 
 const NoteCard = ({ title, body }) => {
   return (
-    <div class="vertical-card-wrapper note-card-wrapper p-2">
-      <div class="vertical-card-header flex flexAlignItemsCenter">
-        <h2 class="m-2">{title}</h2>
-        <i class="fa-solid fa-thumbtack mx-3"></i>
+    <div className="vertical-card-wrapper note-card-wrapper p-2">
+      <div className="vertical-card-header flex flexAlignItemsCenter">
+        <h2 className="m-2">{title}</h2>
+        <i className="fa-solid fa-thumbtack mx-3"></i>
       </div>
-      <div class="vertical-card-body my-4 mx-2">
+      <div className="vertical-card-body my-4 mx-2">
         <p>{body}</p>
       </div>
       <div className="note-card-footer flex flexAlignItemsCenter">
         <p className="ml-2">timestamp</p>
         <div className="note-card-options">
-          <i class="fa-solid fa-pencil mx-3"></i>
-          <i class="fa-solid fa-palette mx-3"></i>
-          <i class="fa-solid fa-tag mx-3"></i>
-          <i class="fa-solid fa-box-archive mx-3"></i>
-          <i class="fa-solid fa-trash mx-3"></i>
+          <i className="fa-solid fa-pencil mx-3"></i>
+          <i className="fa-solid fa-palette mx-3"></i>
+          <i className="fa-solid fa-tag mx-3"></i>
+          <i className="fa-solid fa-box-archive mx-3"></i>
+          <i className="fa-solid fa-trash mx-3"></i>
         </div>
       </div>
     </div>
--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -6,21 +6,21 @@ const NoteForm = () => {
       <form className="note-form-wrapper my-4">
         <div className="flex flexAlignItemsCenter">
           <textarea className="note-form-title" placeholder="Title" />
-          <i class="fa-solid fa-thumbtack mx-3"></i>
+          <i className="fa-solid fa-thumbtack mx-3"></i>
         </div>
         <textarea className="note-form-body" placeholder="Take a note..." />
         <div className="flex flexAlignItemsCenter">
           <button
             type="submit"
-            class="btn btn-outline-primary add-note-btn m-2"
+            className="btn btn-outline-primary add-note-btn m-2"
           >
             Add Note
           </button>
           <div className="note-form-options">
-            <i class="fa-solid fa-palette mx-3"></i>
-            <i class="fa-solid fa-tag mx-3"></i>
-            <i class="fa-solid fa-box-archive mx-3"></i>
-            <i class="fa-solid fa-trash mx-3"></i>
+            <i className="fa-solid fa-palette mx-3"></i>
+            <i className="fa-solid fa-tag mx-3"></i>
+            <i className="fa-solid fa-box-archive mx-3"></i>
+            <i className="fa-solid fa-trash mx-3"></i>
           </div>
         </div>
       </form>
