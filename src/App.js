99df2a9da93914a994d583f2620b53d7f677fb90
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-05 00:50:01 +0530

    style: reduce autoclose time of toast

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
     <>
       <ToastContainer
         position="top-right"
-        autoClose="5000"
+        autoClose="3000"
         hideProgressBar="false"
         closeOnClick="true"
         pauseOnHover="true"
