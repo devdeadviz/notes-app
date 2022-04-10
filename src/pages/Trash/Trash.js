03ebb3e7c404e15a636fba40de37e7a03951c06f
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-11 02:34:14 +0530

    feat: add searchinput and map trash array in trash page

--- src/pages/Trash/Trash.js
+++ src/pages/Trash/Trash.js
@@ -1,12 +1,21 @@
-import { Sidebar } from "../../components";
+import { NoteCard, SearchInput, Sidebar } from "../../components";
+import { useNote } from "../../contexts";
 import "./Trash.css";
 
 const Trash = () => {
+  const {
+    noteState: { trashNotes },
+    noteDispatch,
+  } = useNote();
+
   return (
     <section className="trash-wrapper flex">
       <Sidebar />
       <section className="trash-main-section flex flexCol flexAlignItemsCenter pt-2">
-        Trash Page
+        <SearchInput />
+        {trashNotes.map((trashNoteData) => (
+          <NoteCard key={trashNoteData._id} trashNote={trashNoteData} />
+        ))}
       </section>
     </section>
   );
