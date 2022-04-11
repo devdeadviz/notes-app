5f8bde28448681709938ee437802e78f05378f02
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-12 00:59:41 +0530

    feat: add trash card component

---
+++ src/components/TrashCard/TrashCard.js
@@ -0,0 +1,24 @@
+import "./TrashCard.css";
+
+const TrashCard = ({ title, body, createdAt }) => {
+  return (
+    <div className="vertical-card-wrapper trash-card-wrapper p-2 my-4">
+      <div className="vertical-card-header flex flexAlignItemsCenter">
+        <h2 className="m-2">{title}</h2>
+        <i className="fa-solid fa-thumbtack mx-3"></i>
+      </div>
+      <div className="vertical-card-body my-4 mx-2">
+        <p>{body}</p>
+      </div>
+      <div className="trash-card-footer flex flexAlignItemsCenter">
+        <p className="trash-card-date ml-2">Deleted on: {createdAt}</p>
+        <div className="trash-card-options">
+          <i class="fa-solid fa-trash-arrow-up mx-3"></i>
+          <i className="fa-solid fa-trash mx-3"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { TrashCard };
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Sidebar } from "./Sidebar/Sidebar";
 export { NoteForm } from "./NoteForm/NoteForm";
 export { NoteCard } from "./NoteCard/NoteCard";
 export { SearchInput } from "./SearchInput/SearchInput";
+export { TrashCard } from "./TrashCard/TrashCard";
