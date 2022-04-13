959e1d870385d97dedb4ce67967c3a63e1d88824
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-13 22:08:20 +0530

    fix: archive note service missing request body

--- src/services/note-services/archiveNote.js
+++ src/services/note-services/archiveNote.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const archiveNote = async (noteId, encodedToken) => {
+const archiveNote = async (note, encodedToken) => {
   try {
-    const { data } = await axios.post(`/api/notes/archives/${noteId}`, {
+    const { data } = await axios.post(`/api/notes/archives/${note._id}`, note, {
       headers: { authorization: encodedToken },
     });
-    return data.notes;
+    return data;
   } catch (error) {
     console.error(error.data);
   }
